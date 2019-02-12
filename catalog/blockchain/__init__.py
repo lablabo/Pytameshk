d3adfa3d40f74ed66d047389e706679c847de409
Sam <lablabo@gmail.com>
2019-02-12 16:40:31 +0000

    create blockchain module

--- catalog/blockchain/__init__.py
+++ catalog/blockchain/__init__.py
@@ -4,4 +4,5 @@ from flask import Flask, jsonify, request
 class controller:
     def __init__(self, action, libraries):
         blockchain = libraries['blockchain']
+        print("first module")
         pass
