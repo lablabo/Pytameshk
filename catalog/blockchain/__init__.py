46cf95a76491cd81934ba6bb8f62fa9e6d231926
Sam <lablabo@gmail.com>
2019-02-12 14:44:36 +0000

    delete file

--- catalog/blockchain/__init__.py
+++ catalog/blockchain/__init__.py
@@ -1,5 +1,5 @@
 class controller:
     def __init__(self, action):
         print(action.config.path)
-        exit("start blockchain13333333")
+        exit("start blockchain133333334")
         pass
