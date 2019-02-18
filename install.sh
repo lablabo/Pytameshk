4120910a717418677b120f8c774404d28b4381f8
Mohammad <lablabo@gmail.com>
2019-02-18 11:52:27 +0000

    Start Installer

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+sudo python system/helper/pip/get-pip.py
 sudo python -m pip --version
-sudo python -m install flash
-sudo python -m install uuid
-sudo python -m install platform
-sudo python -m install numpy
-sudo python -m install socket
-sudo python -m install itertools
-sudo python -m install re
-sudo python -m install requests
+sudo python -m pip install flash
+sudo python -m pip install uuid
+sudo python -m pip install platform
+sudo python -m pip install numpy
+sudo python -m pip install socket
+sudo python -m pip install itertools
+sudo python -m pip install re
+sudo python -m pip install requests
