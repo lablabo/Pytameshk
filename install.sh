0bec11023604e009da5e4c8cc40f522433547bda
Mohammad <lablabo@gmail.com>
2019-02-25 15:04:12 +0000

    Sorvin & Indy

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-sudo python system/helper/pip/get-pip.py
-sudo python -m pip --version
-sudo python -m pip install flash
-sudo python -m pip install uuid
-sudo python -m pip install platform
-sudo python -m pip install numpy
-sudo python -m pip install socket
-sudo python -m pip install itertools
-sudo python -m pip install re
-sudo python -m pip install requests
-python -m pip install cryptography
+sudo python3 system/helper/pip/get-pip.py
+sudo python3 -m pip --version
+sudo python3 -m pip install flash
+sudo python3 -m pip install uuid
+sudo python3 -m pip install platform
+sudo python3-m pip install numpy
+sudo python3 -m pip install socket
+sudo python3 -m pip install itertools
+sudo python3 -m pip install re
+sudo python3 -m pip install requests
+sudo python3 -m pip install cryptography
 
 print "------ Installer Ended ------"
 sudo python index.py
