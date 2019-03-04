9475f99f401587f5cc53cf8cd920826afd8c114e
Mohammad <lablabo@gmail.com>
2019-03-04 12:22:06 +0000

    Engine - Exception

--- catalog/IOTA/__init__.py
+++ catalog/IOTA/__init__.py
@@ -10,42 +10,11 @@ from six import text_type
 
 from iota import BadApiResponse, StrictIota, __version__
 
+
 class controller:
 
     def __init__(self, action, libraries):
         print("Start IOTA")
-        '''
-        parser = ArgumentParser(
-            description=__doc__,
-            epilog='PyOTA v{version}'.format(version=__version__),
-        )
 
-        parser.add_argument(
-            '--uri',
-            type=text_type,
-            default='http://localhost:14265/',
 
-            help=(
-                'URI of the node to connect to '
-                '(defaults to http://localhost:14265/).'
-            ),
-        )
-        '''
 
-        #self.main(**vars(parser.parse_args(argv[1:])))
-
-    def main(self, uri):
-        api = StrictIota(uri)
-        try:
-            node_info = api.get_node_info()
-        except ConnectionError as e:
-            print(
-                "Hm.  {uri} isn't responding; is the node running?".format(uri=uri)
-            )
-            print(e)
-        except BadApiResponse as e:
-            print("Looks like {uri} isn't very talkative today ):".format(uri=uri))
-            print(e)
-        else:
-            print('Hello {uri}!'.format(uri=uri))
-            pprint(node_info)
