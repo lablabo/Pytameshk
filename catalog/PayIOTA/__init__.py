617d74b3c4bce9a1ba88b87cd6401e37afc9fe2f
Sam <lablabo@gmail.com>
2019-05-29 17:03:02 +0100

    sqlite Server and RestFul API

--- catalog/PayIOTA/__init__.py
+++ catalog/PayIOTA/__init__.py
@@ -1,20 +1,24 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import zmq
 from zmq.utils.monitor import recv_monitor_message
-from iota import TryteString
-
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+from flask import Flask, jsonify, request, Response
+from multiprocessing import Value
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
 
 class controller:
+    iota = None
     def __init__(self, action, libraries, settings):
-
-        iota = libraries['iota']
-
-        # Set API URL Call
-        # You can change it in setup.ini
-        iota.setApiCall(settings['DEFAULT']['API_CALL'])
-
-        #iota.do_prepare_transaction()
-        #self.testZMG(iota)
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_resource(Employees_Name, '/api/<address>', resource_class_kwargs={'action': action,
+                                                                                  'libraries': libraries,
+                                                                                  'settings': settings})
+        app.run(port='5002')
 
     def testZMG(self, iota):
         context = zmq.Context()
@@ -39,3 +43,24 @@ class controller:
             data = iota.decode_tryte_method_2(topic[1].encode('utf-8'))
             # More Information https://pyota.readthedocs.io/en/latest/types.html
             print(data)
+
+
+class Employees_Name(Resource):
+    def __init__(self, action, libraries, settings):
+        self.settings = settings
+        self.action = action
+        self.libraries = libraries
+
+    def get(self, address):
+        self.db_connect = create_engine('sqlite:///chinook.db')
+        self.iota = self.libraries['iota']
+
+        # Set API URL Call
+        # You can change it in setup.ini
+        self.iota.setApiCall(self.settings['DEFAULT']['API_CALL'])
+
+        return jsonify({"number": address})
+
+        # conn = db_connect.connect()
+        # query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
+        # result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
